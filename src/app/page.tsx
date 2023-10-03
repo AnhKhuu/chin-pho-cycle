1ad6d9f45546d22b5b2beff7e14b43217bb1fb63
Aster <trangnminh.98@gmail.com>
2023-10-03 11:20:39 +0700

    change button import path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return <Button className="m-5">Please kill me</Button>;
