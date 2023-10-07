ce507b93d0f95dab37c260cec643c5f500921640
Trang Nguyen <tea@Mithril.local>
2023-10-07 13:35:36 +0700

    chore: change naming convention

--- src/app/(root)/(store)/(routes)/page.tsx
+++ src/app/(root)/(store)/(routes)/page.tsx
@@ -22,7 +22,7 @@ const formSchema = z.object({
   }),
 });
 
-export default function StorePage() {
+export default function Page() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
