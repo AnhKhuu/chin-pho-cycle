87871f836f71cdb2c81a1bdb1bd462bd8c44d737
Trang Nguyen <trangnminh.98@gmail.com>
2023-10-22 00:32:08 +0700

    change create brand url

--- src/app/(root)/admin/(routes)/brands/components/client.tsx
+++ src/app/(root)/admin/(routes)/brands/components/client.tsx
@@ -19,7 +19,7 @@ export const BrandClient: React.FC<IBrandsClientProps> = ({ data }) => {
           title={`Brands ${data ? `(${data.length})` : ''}`}
           description='Manage brands for your store'
         />
-        <Link href={`${AdminRoutes.BRANDS}/new`}>
+        <Link href={`${AdminRoutes.BRANDS}/create`}>
           <Button>
             <Plus className='mr-2 h-4 w-4' />
             Add New
